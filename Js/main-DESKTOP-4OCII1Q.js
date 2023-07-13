4a2fd55bafb7e9e37e4487e3bc377f8bd87a464d
repomariangel <pachecoriveromariangel@gmail.com>
2023-07-13 20:22:02 -0300

    modified:   Estilos/estilos.css 	modified:   Js/main.js 	modified:   index.html

---
+++ Js/main-DESKTOP-4OCII1Q.js
@@ -0,0 +1,14 @@
+ /*iniciomenuItems.forEach(function(item) {
+   item.addEventListener('clic', function(e) {
+    const currentItem = document.querySelector ('.active');
+        currentItem.classList.remove('.active');
+        e.target.classList.add('active');
+    });
+});
+*/
+
+
+   let = document.querySelector ('tele')
+   addEventListener('clic', function(e) {
+         e.target.classList.toggle('tele');
+     });
--- Js/main.js
+++ Js/main.js
@@ -1,31 +1,20 @@
-let parrafo = document.querySelector ('#tele');
-parrafo.addEventListener('click', function(e) {
-e.target.classList.toggle('parrafo-modificado'); });
+ /*iniciomenuItems.forEach(function(item) {
+   item.addEventListener('clic', function(e) {
+    const currentItem = document.querySelector ('.active');
+        currentItem.classList.remove('.active');
+        e.target.classList.add('active');
+    });
+});
+*/
 
+/*
+   let ul = document.querySelector ('#tele')
+   ul.addEventListener('clic', function(e) {
+         e.target.classList.toggle('#tele');
+     });*/
+function saludar(nombre,apellido){
+    console.log("Contáctame"+"1164773949");
 
-/*let logos= [
-{nombre:'Email',
-img:'/Imagenes/mail.png'},
-{nombre:'Github',
-img:'/Imagenes/github.png'},
-{nombre:'Linkedin',
-img:'/Imagenes/linkedin.png'},];
-
-function crearHTMLLogo(logo) {
-    let nombreLogo= document.createElement('h3');
-    nombreLogo.innerText = logo.nombre;        
-    let imagenLogo = document.createElement('img');
-    imagenLogo.src = logo.img;
-
-    let listItem = document.createElement('li');
-    listItem.appendChild(nombreLogo);
-    listItem.appendChild(imagenLogo);
-    return listItem;}
-
-const listaLogos = document.querySelector('#logo-list');
-
-logos.forEach(function(logo) {
-let logoListItem = 
-  crearHTMLLogo(logo); 
-  listaLogos.appendChild(logosListItem);
-});*/
+    var divSaludor=document.getElementeById("tele");
+    divSaludar.innerText="Contáctame"+ + nombre+""+ apellido;
+}
